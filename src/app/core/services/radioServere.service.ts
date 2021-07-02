8e30d37be4bbee81d5e8b4857a0cb9129ffd7194
JuanMa Velez <juanmanuelveleztorres@gmail.com>
2021-07-02 10:53:15 +0200

    radioServere.service : adding types to getAllRadios

--- src/app/core/services/radioServere.service.ts
+++ src/app/core/services/radioServere.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { IResponseRadioServer } from 'src/app/models';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,7 @@ export class RadioServereService {
     this.serverUrl = 'https://teclead.de/recruiting/radios';
   }
 
-  getAllRadios(): any {
-    return this.http.get<any>(this.serverUrl);
+  getAllRadios(): Observable<IResponseRadioServer> {
+    return this.http.get<IResponseRadioServer>(this.serverUrl);
   }
 }
